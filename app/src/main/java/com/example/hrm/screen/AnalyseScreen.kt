eac0e40481a6cb67955d7b4e488e16e0376a0fa9
Jiayue <jiayue.zh@outlook.com>
2025-05-18 15:03:57 +0800

    feat: Remove unused imports in AnalyseScreen for cleaner code

--- app/src/main/java/com/example/hrm/screen/AnalyseScreen.kt
+++ app/src/main/java/com/example/hrm/screen/AnalyseScreen.kt
@@ -3,13 +3,8 @@ package com.example.hrm.screen
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,13 +17,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.hrm.db.HealthViewModel
 import com.example.hrm.db.entity.GeneralPhysical
-import com.example.hrm.component.RowItem
 import com.example.hrm.component.showcard.BloodDetails
 import com.example.hrm.component.showcard.GeneralPhysicalDetails
 import com.example.hrm.component.showcard.LiverDetails
